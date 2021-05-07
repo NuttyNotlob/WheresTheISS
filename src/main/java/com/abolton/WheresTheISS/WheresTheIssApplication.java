f4f6e70a69933a15918dd3e537cdba1b9491bd1a
LAPTOP-NN5A72M9\alex- <boltonalex89@gmail.com>
2021-05-07 11:18:10 +0100

    Initial commit

---
+++ src/main/java/com/abolton/WheresTheISS/WheresTheIssApplication.java
@@ -0,0 +1,13 @@
+package com.abolton.WheresTheISS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheresTheIssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheresTheIssApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/abolton/WheresTheISS/services/ISSLocationService.java
@@ -0,0 +1,7 @@
+package com.abolton.WheresTheISS.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ISSLocationService {
+}
